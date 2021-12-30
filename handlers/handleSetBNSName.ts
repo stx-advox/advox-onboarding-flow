a7964e74c8861c92ec12cf2f1845579ff40ae4b1
hz <medo3578@gmail.com>
2021-12-30 19:36:18 +0200

    better UX handling of errors

--- handlers/handleSetBNSName.ts
+++ handlers/handleSetBNSName.ts
@@ -17,29 +17,28 @@ export const handleSetBNSName = (client: Client, ledger: any) => {
       const isNameValid = await checkNameValid(name);
       const nameEscaped = name.replace(/\./g, "-");
       const isNameSet = ledger.accountByName(nameEscaped);
-      if (!isNameValid || isNameSet) {
-        message.reply(
-          "hey dude that name is not owned by any address, or is already registered, could you check again?"
-        );
-      } else {
+      const discordAccount = ledger.accountByAddress(
+        `N\u0000sourcecred\u0000discord\u0000MEMBER\u0000user\u0000${message.author.id}\u0000`
+      );
+
+      if (isNameValid && isNameSet && discordAccount) {
         message.reply(
           "Aight, we'll use this bns name to send you your rewards!"
         );
 
         // get the ledger using github
-        const discordAccount = ledger.accountByAddress(
-          `N\u0000sourcecred\u0000discord\u0000MEMBER\u0000user\u0000${message.author.id}\u0000`
-        );
 
-        if (!discordAccount) {
-          message.reply(
-            "Sorry dude, did you just join the server today? try again tomorrow!"
-          );
-          return;
-        }
         // update the name of the identity related with the author's id with the new bns name
         ledger.renameIdentity(discordAccount.identity.id, nameEscaped);
         ledger.activate(discordAccount.identity.id);
+      } else if (!discordAccount) {
+        message.reply(
+          "Sorry dude, did you just join the server today? try again tomorrow!"
+        );
+      } else {
+        message.reply(
+          "hey dude that name is not owned by any address, or is already registered, could you check again?"
+        );
       }
     }
   });
