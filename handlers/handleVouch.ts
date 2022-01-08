48df3d5f32f6cdae156a8df229a6d503ea7f2db7
hz <medo3578@gmail.com>
2022-01-08 18:05:15 +0200

    vouch now directs people to join requests

--- handlers/handleVouch.ts
+++ handlers/handleVouch.ts
@@ -1,6 +1,7 @@
 import { Client, Message } from "discord.js";
 import {
   ADVOCATE_ROLE,
+  JOIN_REQUESTS_CHANNEL,
   TEMP_ADVOCATE_ROLE,
   VOUCH_CHANNEL,
 } from "../util/constants";
@@ -16,9 +17,11 @@ export const handleVouch = async (client: Client) => {
         const mentions = message.mentions;
         const member = await stacks.members.fetch(message.author.id);
         if (member.roles.cache.has(ADVOCATE_ROLE) && mentions.members) {
-          for (let member of mentions.members.values()) {
-            await member.roles.add(TEMP_ADVOCATE_ROLE);
-          }
+          const allVouched = Array.from(mentions.members.values())
+            .map((a) => `<@${a.id}>`)
+            .join(" ");
+          message.reply(`${allVouched}, glad to have you on board!
+could you please give us an introduction in <#${JOIN_REQUESTS_CHANNEL}>`);
         }
       }
     } catch (error) {
