10ab57f29bd5559121262cfa6c07d149eed9d37d
hz <13457698+hozzjss@users.noreply.github.com>
2022-05-09 17:46:52 +0200

    fix update grain job - the job didn't post content correctly to the config

--- cron/updateGrainDistCron.ts
+++ cron/updateGrainDistCron.ts
@@ -19,48 +19,50 @@ const sourcecredInstance = getGithubWriteInstance(
 const GRAIN_PATH: Array<string> = ["config", "grain.json"];
 const grainConfigPath = path.join(...GRAIN_PATH);
 
+const updateGrainJob = async () => {
+  const infoData = await getPoXInfo();
+  const [info] = await GrainInfo.find({}).exec();
+  const lastCycleId = infoData.current_cycle.id - 1;
+  if (!info) {
+    await new GrainInfo({
+      lastCycleId,
+    }).save();
+    return;
+  }
+  if (info.lastCycleId < lastCycleId) {
+    const stackerData = await getStackerData(lastCycleId);
+    const lastCycleRewards =
+      stackerData.blockRewards.aggregate.sum.reward_amount;
+    const xBTCTotal = lastCycleRewards * 0.998;
+    const weekDistribution = Math.floor(xBTCTotal * 0.42);
+    const grainConfig: IGrainConfig = JSON.parse(
+      new TextDecoder().decode(
+        await sourcecredInstance._writableStorage.get(grainConfigPath)
+      )
+    );
+    const [allocation] = grainConfig.allocationPolicies;
+    allocation.budget = weekDistribution.toString();
+    const newConfig: IGrainConfig = {
+      ...grainConfig,
+      allocationPolicies: [allocation],
+    };
+
+    await sourcecredInstance._writableStorage.set(
+      grainConfigPath,
+      new TextEncoder().encode(stringify(newConfig, { space: 2 })),
+      `Update grain amount last rewards ${lastCycleRewards / 1e8} BTC`,
+      AUTHOR
+    );
+
+    info.lastCycleId = lastCycleId;
+    await info.save();
+  }
+};
+
 export const updateGrainDistCron = () => {
   scheduler.scheduleJob(
     "update the xBTC amount distributed each week, happens every monday",
     "0 0 * * 1",
-    async () => {
-      const infoData = await getPoXInfo();
-      const [info] = await GrainInfo.find({}).exec();
-      const lastCycleId = infoData.current_cycle.id - 1;
-      if (!info) {
-        await new GrainInfo({
-          lastCycleId,
-        }).save();
-        return;
-      }
-      if (info.lastCycleId < lastCycleId) {
-        const stackerData = await getStackerData(lastCycleId);
-        const lastCycleRewards =
-          stackerData.blockRewards.aggregate.sum.reward_amount;
-        const xBTCTotal = lastCycleRewards * 0.998;
-        const weekDistribution = xBTCTotal * 0.42;
-        const grainConfig: IGrainConfig = JSON.parse(
-          new TextDecoder().decode(
-            await sourcecredInstance._writableStorage.get(grainConfigPath)
-          )
-        );
-        const [allocation] = grainConfig.allocationPolicies;
-        allocation.budget = weekDistribution.toString();
-        const newConfig: IGrainConfig = {
-          ...grainConfig,
-          allocationPolicies: [allocation],
-        };
-
-        await sourcecredInstance._writableStorage.set(
-          grainConfigPath,
-          stringify(newConfig, { space: 2 }),
-          `Update grain amount last rewards ${lastCycleRewards / 1e8} BTC`,
-          AUTHOR
-        );
-
-        info.lastCycleId = lastCycleId;
-        await info.save();
-      }
-    }
+    updateGrainJob
   );
 };
