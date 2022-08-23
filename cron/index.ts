8c0c0b479aeeee8674a6a2fdfbc7559942932015
hz <13457698+hozzjss@users.noreply.github.com>
2022-08-23 22:45:53 +0200

    remove updates to grain dist

--- cron/index.ts
+++ cron/index.ts
@@ -1,7 +1,7 @@
 import { bnsNameCron } from "./bnsNameCron";
-import { updateGrainDistCron } from "./updateGrainDistCron";
+// import { updateGrainDistCron } from "./updateGrainDistCron";
 
 export const setupCronJobs = () => {
   bnsNameCron();
-  updateGrainDistCron();
+  // updateGrainDistCron();
 };
